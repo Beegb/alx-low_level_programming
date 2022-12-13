3f754981c7236b986b90045aa38de3da68d91d29
Beegb <adegbenleb@gmail.com>
2022-12-13 09:39:20 -0800

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,42 @@
-#include <unistd.h>
-
 #include "main.h"
 
-
-
-/**
- *
- *  * main - Check description
- *
- *   * Description: It prints the word Holberton, followed by a new line.
- *
- *    * Return: 0.
- *
- *     */
-
-int main(void)
-
-{
-
-		char word[9] = "main";
-
-			int i;
+	/**
+	 *
+	 * 	 * main - print _putchar
+	 *
+	 * 	 	 * is written by mbah
+	 *
+	 * 	 	 	 * Return: 0
+	 *
+	 * 	 	 	 	 */
 
 
 
-				for (i = 0; i < 9; i++)
-
-							_putchar(word[i]);
-
-					_putchar('\n');
 
 
+	int main(void)
 
-						return (0);
+		{
 
-}
+				_putchar('_');
+
+					_putchar('p');
+
+						_putchar('u');
+
+							_putchar('t');
+
+								_putchar('c');
+
+									_putchar('h');
+
+										_putchar('a');
+
+											_putchar('r');
+
+												_putchar('\n');
+
+													return (0);
+
+														}
+
